a7533939f7b42baaee4f728f7cc8fad99b6541d5
Diosimar Moreno <morenodiosimar@gmail.com>
2026-06-20 16:03:11 -0400

    Bd correcta

--- nutriexpert.sql
+++ nutriexpert.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-06-2026 a las 23:40:00
+-- Tiempo de generación: 20-06-2026 a las 21:49:55
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `evaluaciones`
+--
+
+CREATE TABLE `evaluaciones` (
+  `id_evaluacion` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `estatura` decimal(5,2) NOT NULL,
+  `peso` decimal(5,2) NOT NULL,
+  `porcentaje_grasa` decimal(4,1) DEFAULT NULL,
+  `nivel_actividad` varchar(50) NOT NULL,
+  `objetivo_principal` varchar(50) NOT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -38,8 +55,22 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `edad`, `sexo`, `contrasena`, `fecha_registro`) VALUES
+(1, 'Diosimar Moreno', 'morenodiosimar@gmail.com', 21, 'F', 'Reina12**', '2026-06-17 22:06:42');
+
+--
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `evaluaciones`
+--
+ALTER TABLE `evaluaciones`
+  ADD PRIMARY KEY (`id_evaluacion`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -53,10 +84,26 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `evaluaciones`
+--
+ALTER TABLE `evaluaciones`
+  MODIFY `id_evaluacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `evaluaciones`
+--
+ALTER TABLE `evaluaciones`
+  ADD CONSTRAINT `evaluaciones_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
